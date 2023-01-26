4b1afd5d2b4bea5d439cd012e84625b167086f1c
Renan Gabriel <116770162+renangabriel08@users.noreply.github.com>
2023-01-26 19:27:45 -0300

    :tada: Primeiro Projeto PhP

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
     $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = 'Rc15348120';
+    $dbUsername = ''; // Colocar entre aspas Nome do Local do banco de dados (Normalmente o padrão é root)
+    $dbPassword = ''; // Colocar entre aspas a senha que você configurou para guardar o banco de dados (caso não colocou seixar vazio)
     $dbName = 'formulario-teste';
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
